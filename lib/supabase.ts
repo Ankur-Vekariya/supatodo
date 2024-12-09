716a7cb84f1eb3c2437cafef892f41ea5dfec123
Ankur-Vekariya <ankurvekariya2001@gmail.com>
2024-12-09 16:27:53 +0530

    Fix anon key type bug

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,10 +3,13 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://lrvxcyfidyzbqwbddjhx.supabase.co";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-console.log(supabaseKey)
+console.log(supabaseKey);
 // if (!supabaseKey) {
 //   throw new Error("Supabase key is not defined.");
 // }
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(supabaseUrl, supabaseKey ?? "");
+export const supabase = createClient(
+  supabaseUrl,
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxydnhjeWZpZHl6YnF3YmRkamh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1NzU5MjEsImV4cCI6MjA0OTE1MTkyMX0.2GNeT800Yg5nMPN_q7RLzm3YkUe-ekIeFJXF-4claLs"
+);
