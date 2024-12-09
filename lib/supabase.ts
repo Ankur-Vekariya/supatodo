98b8aa1dace3c76462e0a743c874c21f009b51fb
Ankur-Vekariya <ankurvekariya2001@gmail.com>
2024-12-09 16:22:45 +0530

    Fix type bug

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,5 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://lrvxcyfidyzbqwbddjhx.supabase.co";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseKey) {
+  throw new Error("Supabase key is not defined.");
+}
+
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(supabaseUrl, supabaseKey);
